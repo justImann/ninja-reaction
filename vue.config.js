6a7de99fd33a57411bb5c6bdea999510922bdca5
Muhammad Nur Iman <94893598+justImann@users.noreply.github.com>
2023-12-26 14:08:24 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/ninja-reaction/public/index.js',
+  // publicPath: '/ninja-reaction/public/index.js',
+    publicPath: process.env.NODE_ENV === "production" ? "/ninja-reaction/" : "/",
   transpileDependencies: true
 })
